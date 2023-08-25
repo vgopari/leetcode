91835ffcfeadce190002e5edeaf7741c6ac2e64c
Vamshi Gopari <vamshigopari6@gmail.com>
2023-08-24 20:48:16 -0400

    SinglyLinkedList

---
+++ src/main/java/com/helloworld/demo/exercise/ListNode.java
@@ -0,0 +1,10 @@
+package com.helloworld.demo.exercise;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/helloworld/demo/exercise/SinglyLinkedList.java
@@ -0,0 +1,76 @@
+package com.helloworld.demo.exercise;
+
+public class SinglyLinkedList {
+
+    int size;
+    ListNode head;
+
+    public SinglyLinkedList() {
+        head = new ListNode(0);
+        size = 0;
+    }
+
+    public int get(int index) {
+
+       if(index < 0 || index >= size)
+           return -1;
+       ListNode curr = head;
+       for(int i = 0; i < index+1; i++) {
+           curr = curr.next;
+       }
+       return curr.val;
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        ListNode prev = head;
+        ListNode toAdd = new ListNode(val);
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        toAdd.next = prev.next;
+        prev.next = toAdd;
+        ++size;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        ListNode pred = head;
+        for (int i = 0; i < index; ++i) pred = pred.next;
+
+        pred.next = pred.next.next;
+        size--;
+    }
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList obj = new SinglyLinkedList();
+
+        obj.addAtHead(1);
+        obj.addAtTail(2);
+        obj.addAtIndex(1, 5);
+//        obj.deleteAtIndex(0);
+
+        ListNode head = obj.head;
+        while (head != null) {
+            System.out.print(head.val + "->");
+            head = head.next;
+        }
+        System.out.println();
+        System.out.println(obj.get(0));
+    }
+}
+
